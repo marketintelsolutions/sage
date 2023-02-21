7633ed7a97c5a46fd626d8ea5dbbd7258681422d
Igee <igbagboleye@gmail.com>
2023-02-21 10:16:21 +0100

    team section completed

--- src/components/SectionThree.js
+++ src/components/SectionThree.js
@@ -3,7 +3,34 @@ import React from "react";
 const SectionThree = () => {
   return (
     <section className="section-three" id="team">
-      Team
+      <h2>Team</h2>
+      <div className="people">
+        <div className="person">
+          <h3 className="name">Olumide Osadugba </h3>
+          <p className="role">CHIEF OPERATING OFFICER (COO) </p>
+          <p className="company">SAGE </p>
+        </div>
+        <div className="person">
+          <h3 className="name">Cladius Eromosele </h3>
+          <p className="role">LABORATORY MANAGER </p>
+          <p className="company">SAGE DIAGNOSTICS </p>
+        </div>
+        <div className="person">
+          <h3 className="name">Adedokun Adedayo Adewunmi </h3>
+          <p className="role">ACCOUNT MANAGER </p>
+          <p className="company">SAGE DIAGNOSTICS </p>
+        </div>
+        <div className="person">
+          <h3 className="name">Dr. Oluwagbogo Christiana Oyebade </h3>
+          <p className="role">MEDICAL OFFICER</p>
+          <p className="company">SAGE DIAGNOSTICS</p>
+        </div>
+        <div className="person">
+          <h3 className="name">Nancy Clement</h3>
+          <p className="role">HR/ADMIN OFFICER</p>
+          <p className="company">AFRILIFE</p>
+        </div>
+      </div>
     </section>
   );
 };
