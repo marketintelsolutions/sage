5947212b7eb83885bb33ac3ac170d7e94a335fbc
igee-17 <igbagboleye@gmail.com>
2023-04-12 13:04:32 +0100

    refresh error fixed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/index.scss";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "react-whatsapp-widget/dist/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
