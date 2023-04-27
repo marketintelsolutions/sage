62d6e5ff4c9ad7f17389e0a1f705aa1986419e04
igee-17 <igbagboleye@gmail.com>
2023-04-27 23:01:15 +0100

    partners done

--- src/components/Patners.js
+++ src/components/Patners.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { partners } from "../utils/partners";
 
 const Patners = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  let interval = 3000;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentIndex === partners.length - 1) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }, interval);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, interval, partners.length]);
+
   return (
     <section className="patners">
-      <h1>partners</h1>
+      <div className="left">
+        <h2>
+          We work with the <br /> best partners
+        </h2>
+        <p>
+          While we are at the forefront and specialize in diagnostics services,
+          we are very familiar with a number of test methods and are confident
+          we can find the service that will best help you meet your
+          expectations.
+        </p>
+      </div>
+      <div className="right">
+        <div className="images">
+          {/* {partners.map((partner, index) => {
+            return (
+              <div key={index} className="partner">
+                <img src={partner.img} alt="partner logo" />
+              </div>
+            );
+          })} */}
+          <img src={partners[currentIndex].img} alt={`Slide ${currentIndex}`} />
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/utils/partners.js
@@ -0,0 +1,19 @@
+import afrilife from "../assets/images/afrilife.png";
+import paclogo from "../assets/images/paclogo.png";
+import philipshmo from "../assets/images/phillipshmo.png";
+import ziltch1 from "../assets/images/ziltch1.png";
+
+export const partners = [
+  {
+    img: afrilife,
+  },
+  {
+    img: paclogo,
+  },
+  {
+    img: philipshmo,
+  },
+  {
+    img: ziltch1,
+  },
+];
